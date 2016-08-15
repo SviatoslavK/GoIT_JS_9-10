a338d9bff019dd8793e92c1fd638febd1a49be05
SviatoslavK <kolodaslav@gmail.com>
2016-08-15 23:33:38 +0300

    added homework

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    $('.navbar__item').hover(function() {
+        $(this).children('.dropdown').stop(false, true).fadeIn(300);
+        $(this).children('a').addClass('selected');
+    }, function() {
+        $(this).children('.dropdown').stop(false, true).fadeOut(300);
+        $(this).children('a').removeClass('selected');
+    });
+
+
+    $('#slider-single').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('select').selectBox({
+        menuSpeed: 'normal',
+        menuTransition: 'fade'
+    });
+
+    $('.checkbox-js').each(function() {
+        var self = $(this),
+            label = self.next(),
+            label_text = label.text();
+
+        label.remove();
+        self.iCheck({
+            checkboxClass: 'icheckbox_line-blue',
+            radioClass: 'iradio_line-blue',
+            insert: '<div class="icheck_line-icon"></div>' + label_text
+        });
+    });
+
+});
